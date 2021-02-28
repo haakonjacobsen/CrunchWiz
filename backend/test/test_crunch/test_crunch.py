ee6c4dc9ac2c31c3e6a344e34315632526d4b4d9
henrikya <henrikya@ntnu.no>
2021-02-28 12:50:00 +0100

    Adds function for computing information processing index, Closes #9

--- backend/test/test_crunch/test_crunch.py
+++ backend/test/test_crunch/test_crunch.py
@@ -1,3 +1,11 @@
+from backend.crunch.eyetracker.measurement_functions import compute_information_processing_index
+
+
 class TestCrunch:
     def test_crunch(self):
-        assert(True)
+        assert (True)
+
+    def test_compute_information_processing_index(self):
+        init = [111, 309, 467, 809, 1308, 1609, 1927, 2088, 2267, 2670, 2967]
+        end = [203, 399, 539, 1219, 1499, 1701, 2044, 2239, 2358, 2940, 3142]
+        compute_information_processing_index(init, end)
