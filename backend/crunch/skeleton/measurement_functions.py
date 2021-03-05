f299b9095882dc281c25e4c2a5b74d088e422f9a
Jan <janmla@stud.ntnu.no>
2021-03-05 01:50:10 +0100

    started on something. not 100% sure its correct

--- backend/crunch/skeleton/measurement_functions.py
+++ backend/crunch/skeleton/measurement_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sympy import symbols
 
 df = pd.read_csv("backend/crunch/skeleton/skeleton-S001.csv")
 
@@ -25,6 +26,38 @@ def norm_by_array(a, b):
     norm_z = (a[2] - b[2]) ** 2
 
     return np.sqrt(norm_x + norm_y + norm_z)
+
+
+t = symbols("t")
+
+
+def func(a, b):
+    vector = [
+        b[0] - a[0],
+        b[1] - a[1],
+        b[2] - a[2],
+    ]
+    x = a[0] + (vector[0]) * t
+    y = a[1] + (vector[1]) * t
+    z = a[2] + (vector[2]) * t
+    return x, y, z
+
+
+def finiteDiff(f, tstart, tend):
+    h = 0.25
+    t0 = tstart
+    t1 = tstart + h
+    t2 = t1 + h
+    t3 = t2 + h
+    t4 = tend
+    diff = (
+        -0.5 * f.subs(t, t0) + f.subs(t, t1) - f.subs(t, t3) + 0.5 * f.subs(t, t4)
+    ) / (h ** 3)
+    return diff
+
+
+x, y, z = func([1, 3, 2], [-5, 0, 4])
+finiteDiff(x, 1, 2)
 
 
 def amount_of_motion(t0, t1):
@@ -70,6 +103,6 @@ def print_amount_of_motion(t):
 
 
 # print measurements for first t seconds
-print_stability_of_motion(20)
-print_most_used_joints(20)
-print_amount_of_motion(20)
+# print_stability_of_motion(20)
+# print_most_used_joints(20)
+# print_amount_of_motion(20)
