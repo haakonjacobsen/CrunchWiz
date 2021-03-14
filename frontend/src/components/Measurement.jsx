d935d9cc2392276d3ab0f517532d6012beb99355
haakonrj <haakonrj@ntnu.no>
2021-03-15 00:03:39 +0100

    (30) Added circle indicators for measurements

--- frontend/src/components/Measurement.jsx
+++ frontend/src/components/Measurement.jsx
@@ -3,10 +3,29 @@ import './Measurement.css';
 import PropTypes from 'prop-types';
 
 export default function Measurment({ name, number, showExtended }) {
+  let circleColor = '#C4C4C4';
+  if (number > 20) {
+    circleColor = '#F45656';
+  } else if (number > 10) {
+    circleColor = '#F4B556';
+  } else if (number > -10) {
+    circleColor = '#C4C4C4';
+  } else if (number > -20) {
+    circleColor = '#B3D2A0';
+  } else {
+    circleColor = '#8BD45F';
+  }
   return (
     <div className="Measurment" role="button" value={name} tabIndex={0} onClick={() => showExtended(name)} onKeyDown={() => showExtended}>
       <div className="Measurment-number-wrapper">
-        <h3 className="Measurment-number">{number}</h3>
+        <h3 className="Measurment-number">
+          {number}
+          {' '}
+          %
+        </h3>
+        <svg className="MeasurementIndicator" height="100%" width="100%">
+          <circle cx="50%" cy="50%" r="20%" fill={circleColor} />
+        </svg>
       </div>
       <h3 className="Measurment-text">{name}</h3>
     </div>
--- frontend/src/components/MeasurementExpansion.jsx
+++ frontend/src/components/MeasurementExpansion.jsx
@@ -28,7 +28,7 @@ export default function MeasurmentExpansion({ name, graphData }) {
           >
             <XAxis dataKey="time" />
             <YAxis />
-            <Line animationEasing="ease-in-out" strokeWidth={3} type="monotone" dataKey="number" />
+            <Line isAnimationActive={false} animationEasing="ease-in-out" strokeWidth={3} type="monotone" dataKey="number" />
             <Tooltip />
             <ReferenceLine strokeWidth={3} y={0} label="Baseline" stroke="#959595" strokeDasharray="7" />
             <ReferenceLine strokeWidth={3} y={10} label="High" stroke="#F6C273" strokeDasharray="7" />
