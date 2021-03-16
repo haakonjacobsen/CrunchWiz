dd84f729fd4272445d4d9dd5f3cab9dffc27b415
henrikya <henrikya@stud.ntnu.no>
2021-03-16 15:37:26 +0100

    Changes in test file structure

--- backend/test/test_eyetracker/test_eyetracker.py
+++ backend/test/test_eyetracker/test_eyetracker.py
@@ -1,4 +1,5 @@
 from backend.crunch.eyetracker.measurement_functions import compute_information_processing_index
+from backend.crunch.empatica.measurement_functions import compute_emotional_regulation, compute_entertainment
 
 
 class TestCrunch:
@@ -8,4 +9,4 @@ class TestCrunch:
     def test_compute_information_processing_index(self):
         init = [111, 309, 467, 809, 1308, 1609, 1927, 2088, 2267, 2670, 2967]
         end = [203, 399, 539, 1219, 1499, 1701, 2044, 2239, 2358, 2940, 3142]
-        compute_information_processing_index(init, end)
+        assert compute_information_processing_index(init, end) > 0
