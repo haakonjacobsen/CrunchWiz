7e26bde0f08090d3da2936f6e70d96defc94ee43
henrikya <henrikya@stud.ntnu.no>
2021-03-18 19:13:14 +0100

    fixes information_processing_index

--- backend/crunch/eyetracker/measurement_functions.py
+++ backend/crunch/eyetracker/measurement_functions.py
@@ -1,6 +1,4 @@
-#  Define functions that compute measurements here
-
-# df = pd.read_csv("it2901-crunchwiz/backend/crunch/eyetracker/ET-data-S001.csv")
+import numpy as np
 
 
 def saccade_duration(start_time2, end_time1):
@@ -8,7 +6,11 @@ def saccade_duration(start_time2, end_time1):
 
 
 def saccade_length(x1, y1, x2, y2):
-    return ((x2-x1)**2 + (y2-y1)**2)**0.5
+    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+
+def fixation_duration(start_time1, end_time1):
+    return end_time1 - start_time1
 
 
 def compute_perceived_difficulty(vals):
@@ -20,54 +22,32 @@ def compute_perceived_difficulty(vals):
     count = 0
     sum = 0
     for i in range(1, len(vals)):
-        sacc_dur = saccade_duration(vals[i][1], vals[i-1][2])
-        sacc_len = saccade_length(vals[i-1][5], vals[i-1][6], vals[i][5], vals[i][6])
-        perceived_difficulty = 1/(1+(sacc_len / sacc_dur))
+        sacc_dur = saccade_duration(vals[i][1], vals[i - 1][2])
+        sacc_len = saccade_length(vals[i - 1][5], vals[i - 1][6], vals[i][5], vals[i][6])
+        perceived_difficulty = 1 / (1 + (sacc_len / sacc_dur))
         count += 1
         sum += perceived_difficulty
 
-    return sum/count
+    return sum / count
 
 
-def fixation_duration(start_time1, end_time1):
-    return end_time1 - start_time1
-
-
-def compute_information_processing_index(list_of_init_times, list_of_end_times):
+def compute_information_processing_index(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy):
     """
-    ratio global/local
-    Take a time window
-    Count the number of the long fixations followed by short saccades
-    and vice versa à local processing
-    Count the number of the short fixations followed by long saccades
-    and vice versa à global processing
-    Compute the ratio
+    Takes a list of init and and a list of end-times from a 10 second time window.
+    Take the 25 percentile for the short (fixation duration/saccade length) and 75
+    percentile for long (fixation duration/saccade length). Preferred window size
+    is 10 seconds.
     """
 
-    # Q: What qualifies as long fixations/saccades?
-    # Q: Preferred window size?
-    # Q: have I understood "vice versa correctly"
-    def is_short(duration):
-        return duration > 100
-
-    window_size = 9
     assert type(list_of_init_times) == type(list_of_end_times) == list
-    assert len(list_of_init_times) >= window_size and len(list_of_end_times) >= window_size
+    assert len(list_of_init_times) == len(list_of_end_times) == len(list_of_fx) == len(list_of_fy)
+    div = []
+    for i in range(min(len(list_of_init_times), len(list_of_end_times)) - 1):
+        current_fixation_duration = fixation_duration(list_of_init_times[i], list_of_end_times[i])
+        current_saccade_length = saccade_length(list_of_fx[i], list_of_fy[i], list_of_fx[i + 1], list_of_fy[i + 1])
+        div.append(current_fixation_duration / current_saccade_length)
 
-    local_ipi, global_ipi = (0, 0)
-    prev_saccade_is_long = False
+    short_treshold = np.percentile(np.asarray(div), 25)
+    long_treshold = np.percentile(np.asarray(div), 75)
 
-    for i in range(window_size - 1):
-        fixation_is_long = is_short(fixation_duration(list_of_init_times[i], list_of_end_times[i]))
-        saccade_is_long = is_short(saccade_duration(list_of_init_times[i + 1], list_of_end_times[i]))
-        if fixation_is_long and not saccade_is_long:
-            local_ipi += 1
-        elif not prev_saccade_is_long and fixation_is_long:
-            local_ipi += 1
-        elif not fixation_is_long and saccade_is_long:
-            global_ipi += 1
-        elif prev_saccade_is_long and not fixation_is_long:
-            global_ipi += 1
-        prev_saccade_is_long = saccade_is_long
-
-    return global_ipi / local_ipi
+    return np.sum(np.asarray(div) < short_treshold) / np.sum(np.asarray(div) > long_treshold)
