228b1b244108479b62ee632477aade04ca80430c
henrikya <henrikya@stud.ntnu.no>
2021-03-19 12:09:01 +0100

    ipi now takes thresholds as parameters

--- backend/crunch/eyetracker/measurement_functions.py
+++ backend/crunch/eyetracker/measurement_functions.py
@@ -6,7 +6,7 @@ def saccade_duration(start_time2, end_time1):
 
 
 def saccade_length(x1, y1, x2, y2):
-    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 
 def fixation_duration(start_time1, end_time1):
@@ -31,9 +31,25 @@ def compute_perceived_difficulty(vals):
     return sum / count
 
 
-def compute_information_processing_index(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy):
+def ipi_helper(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy):
     """
-    Takes a list of init and and a list of end-times from a 10 second time window.
+    Returns a list of (fixation duration/saccade length)
+    Used by both compute_information_processing_index and compute_ipi_thresholds.
+    """
+    div = []
+    for i in range(min(len(list_of_init_times), len(list_of_end_times)) - 1):
+        current_fixation_duration = fixation_duration(list_of_init_times[i], list_of_end_times[i])
+        current_saccade_length = saccade_length(list_of_fx[i], list_of_fy[i], list_of_fx[i + 1], list_of_fy[i + 1])
+        div.append(current_fixation_duration / current_saccade_length)
+    return div
+
+
+def compute_information_processing_index(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy,
+                                         short_threshold, long_threshold):
+    """
+    Takes 4 lists as input about fixation start and finish times, and coordinates of the gaze.
+    It takes 2 float values as threshold values, made by compute_ipi_thresholds.
+     The time windows should be 10 second about 10 seconds.
     Take the 25 percentile for the short (fixation duration/saccade length) and 75
     percentile for long (fixation duration/saccade length). Preferred window size
     is 10 seconds.
@@ -41,13 +57,20 @@ def compute_information_processing_index(list_of_init_times, list_of_end_times, 
 
     assert type(list_of_init_times) == type(list_of_end_times) == list
     assert len(list_of_init_times) == len(list_of_end_times) == len(list_of_fx) == len(list_of_fy)
-    div = []
-    for i in range(min(len(list_of_init_times), len(list_of_end_times)) - 1):
-        current_fixation_duration = fixation_duration(list_of_init_times[i], list_of_end_times[i])
-        current_saccade_length = saccade_length(list_of_fx[i], list_of_fy[i], list_of_fx[i + 1], list_of_fy[i + 1])
-        div.append(current_fixation_duration / current_saccade_length)
+    div = ipi_helper(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy)
 
-    short_treshold = np.percentile(np.asarray(div), 25)
-    long_treshold = np.percentile(np.asarray(div), 75)
+    return np.sum(np.asarray(div) < short_threshold) / np.sum(np.asarray(div) > long_threshold)
 
-    return np.sum(np.asarray(div) < short_treshold) / np.sum(np.asarray(div) > long_treshold)
+
+def compute_ipi_thresholds(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy):
+    """
+    Computes threshold values used in ipi. Should be used on the baseline.
+    """
+    assert type(list_of_init_times) == type(list_of_end_times) == list
+    assert len(list_of_init_times) == len(list_of_end_times) == len(list_of_fx) == len(list_of_fy)
+    div = ipi_helper(list_of_init_times, list_of_end_times, list_of_fx, list_of_fy)
+
+    short_threshold = np.percentile(np.asarray(div), 25)
+    long_threshold = np.percentile(np.asarray(div), 75)
+    return short_threshold, long_threshold
+
