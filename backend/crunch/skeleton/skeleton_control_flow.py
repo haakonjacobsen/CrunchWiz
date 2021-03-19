b9f779826ef73067e484da9e8a5b4b8c62d03754
Jan <janmla@stud.ntnu.no>
2021-03-19 01:42:17 +0100

    refactored writecsv to be in control flow

--- backend/crunch/skeleton/skeleton_control_flow.py
+++ backend/crunch/skeleton/skeleton_control_flow.py
@@ -1,12 +1,40 @@
-from .measurement_functions import writeCSV
+import pandas as pd
+import measurement_functions as mf
 
 
-def calculate_measurements(tstart, tend):
-    """
-    Write measurement from skeleton data
-    from x seconds to y seconds
-    """
-    writeCSV(tstart, tend)
+def writeCSV(t0, t1):
+    """ Main function to write measurements to csv """
+    timeArray = []
+    motionArray = []
+    fatigueArray = []
+    stabilityArray = []
+    jointArray = []
+    valueArray = []
+    used_joints_list = [0] * 25
+    i = t0
+    while i < t1:
+        j = i + 1
+        timeArray.append(j)
+        motionArray.append(mf.amount_of_motion(i, j))
+        stabilityArray.append(mf.stability_of_motion(i, j))
+        fatigueArray.append(mf.fatigue(i, j))
+        mf.most_used_joints(i, j, used_joints_list)
+        value = max(used_joints_list)
+        jointArray.append(used_joints_list.index(value))
+        valueArray.append(value)
+        i += 1
+    dict = {
+        "time": timeArray,
+        "motion": motionArray,
+        "stability": stabilityArray,
+        "fatigue": fatigueArray,
+        "most used joint": jointArray,
+        "value for most used joint": valueArray,
+    }
+    df = pd.DataFrame(dict)
+    # mode="a" appends, so we can add new data instead of wiping every time
+    # default path, change accordingly
+    df.to_csv("backend/crunch/skeleton/data/Skeleton.csv", mode="a", index=False)
 
 
 def skeleton_main():
@@ -18,4 +46,7 @@ def skeleton_main():
     """
     # TODO: Refactor to read data and write pdf here instead of in measurement_functions
     print("skeleton process succesfully started")
-    calculate_measurements(50, 60)
+    writeCSV(50, 60)
+
+
+skeleton_main()
