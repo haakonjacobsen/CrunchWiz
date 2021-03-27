f81dfb7eafecc1963dfc83c72f5a4cca564bde87
aron <aron130898@gmail.com>
2021-03-27 14:09:43 +0100

    refactored perceived_diffculty and anticipation functions

--- backend/crunch/eyetracker/measurements/anticipation.py
+++ backend/crunch/eyetracker/measurements/anticipation.py
@@ -1,101 +1,67 @@
-# TODO change from class to functions, and function should take in lists of raw data
+def saccade_duration(start_time2, end_time1):
+    return start_time2 - end_time1
 
-class eyetracker:
-    """The eyetracker class calculates various measurements based on the saccade and fixations"""
 
-    # data points
-    vals = []
+def saccade_length(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-    avg_speed = 0
-    variance = 0
 
-    "Appends new data points and checks if there is enough data to compute measurements"
+def average_speed(initTime, endTime, fx, fy):
+    count = 0
+    speed_sum = 0
+    for i in range(1, len(initTime)):
+        sacc_dur = saccade_duration(initTime[i], endTime[i - 1])
+        sacc_len = saccade_length(
+            fx[i - 1],
+            fy[i - 1],
+            fx[i],
+            fy[i],
+        )
+        sacc_speed = sacc_len / sacc_dur
+        speed_sum += sacc_speed
+        count += 1
+    return speed_sum / count
 
-    def updateVals(self, vals):
-        enough_vals = -1
-        for i in range(len(vals)):
-            self.vals.append(vals[i])
-        if len(self.vals) > 4:
-            enough_vals = 1
-        return enough_vals
 
-    def saccade_duration(self, start_time2, end_time1):
-        return start_time2 - end_time1
+def variance(initTime, endTime, fx, fy, avg_speed):
+    sum_square_difference = 0
+    count = 0
+    for i in range(1, len(initTime)):
+        sacc_dur = saccade_duration(initTime[i], endTime[i - 1])
+        sacc_len = saccade_length(
+            fx[i - 1],
+            fy[i - 1],
+            fx[i],
+            fy[i],
+        )
+        sacc_speed = sacc_len / sacc_dur
+        sum_square_difference += (sacc_speed - avg_speed) ** 2
+        count += 1
+    return sum_square_difference / (count - 1)
 
-    def saccade_length(self, x1, y1, x2, y2):
-        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-    def print_variance(self):
-        print(self.variance)
+def compute_anticipation(initTime, endTime, fx, fy):
+    avg_speed = average_speed(initTime, endTime, fx, fy)
+    var = variance(initTime, endTime, fx, fy, avg_speed)
+    count = 0
+    sum_cube_difference = 0
+    for i in range(1, len(initTime)):
+        sacc_dur = saccade_duration(initTime[i], endTime[i - 1])
+        sacc_len = saccade_length(
+            fx[i - 1],
+            fy[i - 1],
+            fx[i],
+            fy[i],
+        )
+        sacc_speed = sacc_len / sacc_dur
+        sum_cube_difference += (sacc_speed - avg_speed) ** 3
+        count += 1
+    return sum_cube_difference / ((count - 1) * (var ** 0.5) ** 3)
 
-    def average_speed(self):
-        count = 0
-        speed_sum = 0
-        for i in range(1, len(self.vals)):
-            sacc_dur = self.saccade_duration(self.vals[i][1], self.vals[i - 1][2])
-            sacc_len = self.saccade_length(
-                self.vals[i - 1][5],
-                self.vals[i - 1][6],
-                self.vals[i][5],
-                self.vals[i][6],
-            )
-            sacc_speed = sacc_len / sacc_dur
-            speed_sum += sacc_speed
-            count += 1
-        self.avg_speed = speed_sum / count
 
-    def variance(self):
-        sum_square_difference = 0
-        count = 0
-        for i in range(1, len(self.vals)):
-            sacc_dur = self.saccade_duration(self.vals[i][1], self.vals[i - 1][2])
-            sacc_len = self.saccade_length(
-                self.vals[i - 1][5],
-                self.vals[i - 1][6],
-                self.vals[i][5],
-                self.vals[i][6],
-            )
-            sacc_speed = sacc_len / sacc_dur
-            sum_square_difference += (sacc_speed - self.avg_speed) ** 2
-            count += 1
-        self.variance = sum_square_difference / (count - 1)
-
-    def compute_perceived_difficulty(self):
-        count = 0
-        sum = 0
-        for i in range(1, len(self.vals)):
-            sacc_dur = self.saccade_duration(self.vals[i][1], self.vals[i - 1][2])
-            sacc_len = self.saccade_length(
-                self.vals[i - 1][5],
-                self.vals[i - 1][6],
-                self.vals[i][5],
-                self.vals[i][6],
-            )
-            pd = 1 / (1 + (sacc_len / sacc_dur))
-            count += 1
-            sum += pd
-        return sum / count
-
-    def compute_anticipation(self):
-        self.average_speed()
-        self.variance()
-        count = 0
-        sum_cube_difference = 0
-        for i in range(1, len(self.vals)):
-            sacc_dur = self.saccade_duration(self.vals[i][1], self.vals[i - 1][2])
-            sacc_len = self.saccade_length(
-                self.vals[i - 1][5],
-                self.vals[i - 1][6],
-                self.vals[i][5],
-                self.vals[i][6],
-            )
-            sacc_speed = sacc_len / sacc_dur
-            sum_cube_difference += (sacc_speed - self.avg_speed) ** 3
-            count += 1
-        return sum_cube_difference / ((count - 1) * (self.variance ** 0.5) ** 3)
-
-    def compute_measurements(self, vals):
-        if self.updateVals(vals) == 1:
-            result = (self.compute_anticipation(), self.compute_perceived_difficulty())
-            self.vals = []
-            return result
+""" Test Values
+initTime = [309,467,809,1308,1609]
+endTime = [399,539,1219,1499,1701]
+fx = [1166,1087,1052,1048,1069]
+fy = [672,653,599,621,664]
+"""
--- backend/crunch/eyetracker/measurements/perceived_difficulty.py
+++ backend/crunch/eyetracker/measurements/perceived_difficulty.py
@@ -1,19 +1,19 @@
-def compute_perceived_difficulty(vals):
-    """
-    calculates percieved difficulty
-    :param vals: TODO: What is vals?
-    :return:
-    """
+
+def compute_perceived_difficulty(initTime, endTime, fx, fy):
     count = 0
     sum = 0
-    for i in range(1, len(vals)):
-        sacc_dur = saccade_duration(vals[i][1], vals[i-1][2])
-        sacc_len = saccade_length(vals[i-1][5], vals[i-1][6], vals[i][5], vals[i][6])
-        perceived_difficulty = 1/(1+(sacc_len / sacc_dur))
+    for i in range(1, len(initTime)):
+        sacc_dur = saccade_duration(initTime[i], endTime[i - 1])
+        sacc_len = saccade_length(
+            fx[i - 1],
+            fy[i - 1],
+            fx[i],
+            fy[i],
+        )
+        pd = 1 / (1 + (sacc_len / sacc_dur))
         count += 1
-        sum += perceived_difficulty
-
-    return sum/count
+        sum += pd
+    return sum / count
 
 
 def saccade_duration(start_time2, end_time1):
@@ -22,3 +22,11 @@ def saccade_duration(start_time2, end_time1):
 
 def saccade_length(x1, y1, x2, y2):
     return ((x2-x1)**2 + (y2-y1)**2)**0.5
+
+
+""" Test Values
+initTime = [309,467,809,1308,1609]
+endTime = [399,539,1219,1499,1701]
+fx = [1166,1087,1052,1048,1069]
+fy = [672,653,599,621,664]
+"""
