98b006d99ff4b4a7e7560550537ef51401fd3340
Jan <janmla@stud.ntnu.no>
2021-04-09 15:15:55 +0200

    fixed import error for skeleton

--- backend/crunch/skeleton/measurements/__init__.py
+++ backend/crunch/skeleton/measurements/__init__.py
@@ -1,4 +1,4 @@
-from crunch.skeleton.measurements.helpers import norm_by_array, finite_diff, array
+from crunch.skeleton.measurements.helpers import norm_by_array, finite_diff
 from crunch.skeleton.measurements.amount_of_motion import amount_of_motion
 from crunch.skeleton.measurements.fatigue import fatigue
 from crunch.skeleton.measurements.most_used_joints import most_used_joints
