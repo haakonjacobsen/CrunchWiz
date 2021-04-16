e637d86c77ac662581151186825a9596e143e34e
Jan <janmla@stud.ntnu.no>
2021-04-16 13:48:51 +0200

    Resolved float error by changing from vectorized equation to linear equation

--- backend/crunch/skeleton/measurements/fatigue.py
+++ backend/crunch/skeleton/measurements/fatigue.py
@@ -18,9 +18,9 @@ def fatigue(n):
     for i in range(len(n) - 1):
         joint_Fatigue = 0.0
         for j in range(len(n[i])):
-            f, g = equation(n[i][j], n[i + 1][j])
+            f = equation(n[i][j], n[i + 1][j])
             joint_Fatigue += np.abs(finite_diff(f, i, i + 1))
-            joint_Fatigue += np.abs(finite_diff(g, i, i + 1))
+        joint_Fatigue
     frameFatigue = joint_Fatigue / total_Joint
     return round(frameFatigue, 6)
 
@@ -42,7 +42,6 @@ def equation(x, y):
     y1 = x[1]
     x2 = y[0]
     y2 = y[1]
-    vector = [x2 - x1, y2 - y1]
-    x = x1 + (vector[0]) * t
-    y = y1 + (vector[1]) * t
-    return x, y
+    m = (y2 - y1) / (x2 - x1)
+    f = m * (t - x1) + y1
+    return f
