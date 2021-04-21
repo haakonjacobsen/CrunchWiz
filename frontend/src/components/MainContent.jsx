5ee74cb24e1c28a66d02eb8b766dc6140f1f69f1
haakonrj <haakonrj@ntnu.no>
2021-04-21 15:46:25 +0200

    Added stats (max, min, average) of measurments

--- frontend/src/components/MainContent.jsx
+++ frontend/src/components/MainContent.jsx
@@ -14,7 +14,8 @@ const MainContent = () => {
   const [loading, toggleLoading] = useState(false);
   const [showExtended, changeExtended] = useState(false);
   const [selectedMeasurment, setMeasurment] = useState('');
-  const [allData, addMoreData] = useState({});
+  const [graphData, addMoreData] = useState({});
+  const [dataStats, addStats] = useState({});
 
   function isValidIpv4Addr(ipAddress) {
     return /^(?=\d+\.\d+\.\d+\.\d+$)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.?){4}$/.test(ipAddress);
@@ -24,17 +25,47 @@ const MainContent = () => {
     if (isValidIpv4Addr(ipAddr) && parseInt(port, 10) >= 0 <= 65535) {
       if (`${ipAddr}:${port}` === ip) {
         setIP(null);
+        toggleLoading(!loading);
+        console.log(ip);
+      } else {
+        toggleLoading(!loading);
+        setIP(`${ipAddr}:${port}`);
       }
-      toggleLoading(!loading);
-      setIP(`${ipAddr}:${port}`);
     } else {
       setError(`${ipAddr}:${port} is not a valid IPv4 address`);
       console.log(`${ipAddr}:${port} is not a valid IPv4 address`);
     }
   }
 
+  function handleStats(measurement, value) {
+    addStats((prevState) => {
+      if (Object.prototype.hasOwnProperty.call(prevState, measurement)) {
+        const copy = prevState[measurement];
+        if (copy.max < value) {
+          copy.max = value;
+        } else if (copy.min > value) {
+          copy.min = value;
+        }
+        copy.avg += (copy.avg * copy.count + value) / (copy.count + 1);
+        copy.count += 1;
+        return {
+          ...prevState,
+          [measurement]: {
+            max: copy.max, min: copy.min, avg: copy.avg, count: copy.count,
+          },
+        };
+      }
+      return {
+        ...prevState,
+        [measurement]: {
+          max: value, min: value, avg: value, count: 1,
+        },
+      };
+    });
+  }
+
   function handleAdd(measurement, newValue) {
-    // Check if measurement already exist in allData, adds to array if true, creates new if false
+    // Check if measurement already exist in graphData, adds to array if true, creates new if false
     addMoreData((prevState) => (Object.prototype.hasOwnProperty.call(prevState, measurement) ? {
       ...prevState,
       [measurement]: [...prevState[measurement], newValue],
@@ -56,6 +87,7 @@ const MainContent = () => {
     setIP(null);
     setStatus(3);
   }
+
   function toggleExtended(measurment) {
     if (measurment === selectedMeasurment) {
       changeExtended(!showExtended);
@@ -69,8 +101,9 @@ const MainContent = () => {
 
   const receiveMessage = (message) => {
     const measurement = JSON.parse(message.data)[0];
-    const dataPoint = { value: measurement.value.toFixed(2), time: measurement.time };
+    const dataPoint = { value: parseFloat(measurement.value.toFixed(2)), time: measurement.time };
     handleAdd(measurement.name, dataPoint);
+    handleStats(measurement.name, dataPoint.value);
   };
 
   useEffect(() => {
@@ -91,7 +124,7 @@ const MainContent = () => {
   return (
     <div className="Main-content">
       <h1>{renderCount.current}</h1>
-      { wsStatus === 3 && Object.keys(allData).length === 0
+      { wsStatus === 3 && Object.keys(graphData).length === 0
         ? (
           <ConnectingPanel
             connectWebsocket={connectWebsocket}
@@ -104,20 +137,23 @@ const MainContent = () => {
         ? (
           <MeasurmentExpansion
             name={selectedMeasurment}
-            graphData={allData[selectedMeasurment]}
+            graphData={graphData[selectedMeasurment]}
+            dataStats={dataStats[selectedMeasurment]}
+            changeExtended={changeExtended}
+            number={graphData[selectedMeasurment][graphData[selectedMeasurment].length - 1].value}
           />
         )
         : null}
-      { wsStatus === 1 && Object.keys(allData).length === 0
+      { wsStatus === 1 && Object.keys(graphData).length === 0
         ? <LoadingMeasurements />
         : (
           <div className="Measurements-info">
-            { Object.keys(allData).length === 0 && wsStatus === 1
+            { Object.keys(graphData).length === 0 && wsStatus === 1
               ? <LoadingMeasurements />
-              : Object.keys(allData).map((name) => (
+              : Object.keys(graphData).map((name) => (
                 <Measurement
                   name={name}
-                  number={allData[name][allData[name].length - 1].value}
+                  number={graphData[name][graphData[name].length - 1].value}
                   showExtended={toggleExtended}
                   key={name}
                 />
--- frontend/src/components/MeasurementExpansion.jsx
+++ frontend/src/components/MeasurementExpansion.jsx
@@ -9,8 +9,12 @@ import {
   Tooltip,
   ReferenceLine,
 } from 'recharts';
+import Indicator from './Indicator';
+import MeasurmentStat from './MeasurementStat';
 
-export default function MeasurmentExpansion({ name, graphData }) {
+export default function MeasurmentExpansion({
+  name, graphData, dataStats, changeExtended, number,
+}) {
   const [time, setTime] = useState(5);
   return (
     <div className="Extended-panel">
@@ -20,24 +24,22 @@ export default function MeasurmentExpansion({ name, graphData }) {
             {name}
           </h3>
           <div className="SVG-indicator">
-            <svg height="100%" viewBox="0 0 130 130" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="65" cy="65" r="65" fill="#D9D9D9" />
-            </svg>
+            <Indicator number={number} />
           </div>
         </div>
-        <div className="SVG-close">
+        <button type="button" className="SVG-close" onClick={() => changeExtended(false)} onKeyDown={() => changeExtended(false)}>
           <svg width="100%" height="100%" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M38.455 32.5532L63.7647 7.24275C65.4118 5.59641 65.4118 2.9345 63.7647 1.28816C62.1184 -0.35818 59.4565 -0.35818 57.8101 1.28816L32.4996 26.5986L7.18993 1.28816C5.54281 -0.35818 2.88168 -0.35818 1.23533 1.28816C-0.411778 2.9345 -0.411778 5.59641 1.23533 7.24275L26.545 32.5532L1.23533 57.8637C-0.411778 59.51 -0.411778 62.172 1.23533 63.8183C2.05581 64.6395 3.1346 65.0521 4.21263 65.0521C5.29066 65.0521 6.36868 64.6395 7.18993 63.8183L32.4996 38.5078L57.8101 63.8183C58.6314 64.6395 59.7094 65.0521 60.7874 65.0521C61.8654 65.0521 62.9435 64.6395 63.7647 63.8183C65.4118 62.172 65.4118 59.51 63.7647 57.8637L38.455 32.5532Z" fill="#757575" />
           </svg>
-        </div>
+        </button>
       </div>
       <div className="Extended-main">
-        <div className="Extended-info">
-          <div className="Extended-info-box">
-            <h3>102%</h3>
-          </div>
+        <div className="Extended-stats">
+          { Object.entries(dataStats).map(([key, value]) => (
+            <MeasurmentStat name={key} value={value} />
+          ))}
         </div>
-        <div className="Extended-graph">
+        <div className="Extended-graph-container">
           <div className="Extended-graph-header">
             <div className="Graph-panel">
               <button type="button" className={`Graph-panel-choice Left ${time === 5 ? 'selected' : ''}`} onClick={() => setTime(5)} onKeyDown={() => setTime(5)}>1 min</button>
@@ -47,24 +49,26 @@ export default function MeasurmentExpansion({ name, graphData }) {
             </div>
             <h3>Current: 100%</h3>
           </div>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={500}
-              height={200}
-              data={graphData.slice(Math.max(graphData.length - time, 1))}
-              margin={{
-                top: 5, right: 20, bottom: 5, left: 0,
-              }}
-            >
-              <XAxis dataKey="time" />
-              <YAxis />
-              <Line isAnimationActive={false} animationEasing="ease-in-out" strokeWidth={3} type="monotone" dataKey="value" />
-              <Tooltip />
-              <ReferenceLine strokeWidth={3} y={0} label="Baseline" stroke="#959595" strokeDasharray="7" />
-              <ReferenceLine strokeWidth={3} y={10} label="High" stroke="#F6C273" strokeDasharray="7" />
-              <ReferenceLine strokeWidth={3} y={-10} label="Low" stroke="#C1E4AB" strokeDasharray="7" />
-            </LineChart>
-          </ResponsiveContainer>
+          <div className="Extended-graph">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width={500}
+                height={200}
+                data={graphData.slice(Math.max(graphData.length - time, 1))}
+                margin={{
+                  top: 5, right: 20, bottom: 5, left: 0,
+                }}
+              >
+                <XAxis dataKey="time" />
+                <YAxis />
+                <Line isAnimationActive={false} animationEasing="ease-in-out" strokeWidth={3} type="monotone" dataKey="value" />
+                <Tooltip />
+                <ReferenceLine strokeWidth={3} y={1} label="Baseline" stroke="#959595" strokeDasharray="7" />
+                <ReferenceLine strokeWidth={3} y={1.1} label="High" stroke="#F6C273" strokeDasharray="7" />
+                <ReferenceLine strokeWidth={3} y={0.9} label="Low" stroke="#C1E4AB" strokeDasharray="7" />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>
     </div>
