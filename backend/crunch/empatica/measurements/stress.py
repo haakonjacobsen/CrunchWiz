bbcd89394a7e6935533549ec62410803e8be1e44
haavapet <haavapet@stud.ntnu.no>
2021-04-23 18:54:50 +0200

    change stress to calculate negative slope of temperature

--- backend/crunch/empatica/measurements/stress.py
+++ backend/crunch/empatica/measurements/stress.py
@@ -1,11 +1,12 @@
+import numpy as np
+
+
 def compute_stress(temps_list):
     """
     Predicts acute stress based on GSR temperature
     :param temps_list: list of temperatures
     :return: returns the overall change in temperature in the list
     """
-    temperature_sum = 0
-    for i, temp in enumerate(temps_list[:-1]):
-        change = temps_list[i+1] - temp
-        temperature_sum += change
-    return float(temperature_sum)
+    slope = np.polyfit([0.25 * i for i in range(len(temps_list))], temps_list, 1)[0]
+    negative_slope = np.negative(slope)
+    return float(negative_slope)
