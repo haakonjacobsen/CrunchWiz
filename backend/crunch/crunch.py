69e37e48953a9098c5a95a401cc6c37e7a166ed3
Ola Fivelstad Smaaberg <olafsm@stud.ntnu.no>
2021-04-23 23:58:09 +0200

    add mock measurements for emotion & anticipation

--- backend/crunch/crunch.py
+++ backend/crunch/crunch.py
@@ -8,16 +8,24 @@ from crunch.websocket import start_websocket
 
 
 def start_processes(mobile):
+    p4 = Process(target=start_websocket)
+    p4.start()
+
     p1 = Process(target=start_empatica)
     p1.start()
 
     p2 = Process(target=start_eyetracker)
     p2.start()
+
     if mobile:
         p3 = Process(target=start_skeleton)
         p3.start()
     else:
-        p3 = Process(target=start_emotion)
-        p3.start()
-
-    start_websocket()
+        from crunch.emotion import mock_emotion
+        from crunch.emotion import mock_anticipation
+        p5 = Process(target=mock_emotion)
+        p5.start()
+        p6 = Process(target=mock_anticipation)
+        p6.start()
+    # start_emotion()
+    #start_websocket()
--- backend/crunch/emotion/api.py
+++ backend/crunch/emotion/api.py
@@ -7,18 +7,34 @@ import crunch.util as util
 
 def start_emotion():
     import cv2 as cv
-
     # Open you default camera
     cap = cv.VideoCapture(0)
     cap.set(cv.CAP_PROP_BUFFERSIZE, 1)
     er = PyEmotion.DetectFace(device='cpu', gpu_id=0)
-
     timer = util.Time()
-
     while True:
         _, frame = cap.read()
         _, emotion = er.predict_emotion(frame)
         util.write_csv("emotion.csv", [timer.delta_time(), emotion])
-
         # only find emotion once every second
         time.sleep(1)
+
+
+def mock_emotion():
+    import random
+    vals = ['Neutral', 'Sad', 'Fear', 'Angry', 'Surprise', 'Happy']
+    timer = util.Time()
+    while True:
+        val = random.choice(vals)
+        util.write_csv("mock_emotion.csv", [timer.delta_time(), val])
+        time.sleep(1)
+
+
+def mock_anticipation():
+    import random
+    vals = ['high', 'medium', 'low']
+    timer = util.Time()
+    while True:
+        val = random.choice(vals)
+        util.write_csv("mock_anticipation.csv", [timer.delta_time(), val])
+    time.sleep(1)
